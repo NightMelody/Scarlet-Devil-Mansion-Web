466619eb6afb9df21c5fc3ba4c7f6b1fe2b7090a
NightMelody <crisjona7acvs@gmail.com>
2026-01-12 21:13:02 -0800

    Fixed 'base:' in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: "/nightmelody.github.io/Scarlet-Devil-Mansion-Web/"
+  base: "/Scarlet-Devil-Mansion-Web/"
 })
